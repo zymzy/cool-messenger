bbb6bab646989e2823bf81d932047b0117fb8864
microQZR <zi.han.meng100@gmail.com>
2021-07-12 09:14:03 -0400

    Enhanced compute efficiency of accessing and adding/deleting from the 'onlineUsers' store on back-end
    Changes:
    * The 'onlineUsers' store was previously implemented as an array. It is now implemented as a 'Set'
    * Usage of the 'onlineUsers' throughout back-end code base has been adjusted for using 'Set'
    * In file 'server/bin/www', the event listener for client socket 'logout' event now disconnects the socket associated with the logged out user to prevent duplicate socket connections when user re-logs in.

--- server/routes/api/messages.js
+++ server/routes/api/messages.js
@@ -35,7 +35,7 @@ router.post("/", async (req, res, next) => {
         user1Id: senderId,
         user2Id: recipientId,
       });
-      if (onlineUsers.includes(sender.id)) {
+      if (onlineUsers.has(sender.id)) {
         sender.online = true;
       }
     }
